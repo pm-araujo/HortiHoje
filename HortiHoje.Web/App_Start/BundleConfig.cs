bdbe58dbcb134857e6655de697a9e1d40b7732a5
sight92 <sight.pedro@gmail.com>
2016-06-09 08:23:41 +0100

    moved splash, smoother animations

--- HortiHoje.Web/App_Start/BundleConfig.cs
+++ HortiHoje.Web/App_Start/BundleConfig.cs
@@ -8,23 +8,79 @@ namespace HortiHoje
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            // App Services
+            bundles.Add(new ScriptBundle("~/bundles/app/services").Include(
+                "~/app/services/datacontext.js",
+                "~/app/services/directives.js",
+                "~/app/services/entityManagerFactory.js",
+                "~/app/services/model.js",
+                "~/app/services/routeMediator.js"
+            ));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            // App
+            bundles.Add(new ScriptBundle("~/bundles/app").Include(
+                "~/app/activity/activities.js",
+                "~/app/admin/admin.js",
+                "~/app/dashboard/dashboard.js",
+                "~/app/layout/shell.js",
+                "~/app/layout/sidebar.js",
+                "~/app/layout/topnav.js",
+                "~/app/login/login.js",
+                "~/app/reporter/reporters.js"
+            ));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+            // Common
+            bundles.Add(new ScriptBundle("~/bundles/common").Include(
+                "~/app/common/common.js",
+                "~/app/common/logger.js",
+                "~/app/common/spinner.js",
+                "~/app/common/bootstrap/bootstrap.dialog.js" // common.boostrap modules
+            ));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            // Bootstrapping
+            bundles.Add(new ScriptBundle("~/bundles/bootstrapping").Include(
+                "~/app/app.js",
+                "~/app/config.js",
+                "~/app/config.exceptionHandler.js",
+                "~/app/config.route.js"
+            ));
+
+            // Breeze Dependencies
+            bundles.Add(new ScriptBundle("~/bundles/breeze").Include(
+                "~/scripts/breeze.debug.js",
+                "~/scripts/breeze.angular.js",
+                "~/scripts/breeze.directives.js",
+                "~/scripts/breeze.saveErrorExtensions.js",
+                "~/scripts/breeze.to$q.shim.js" // Needed only if using to$q
+            ));
+
+            // Vendor Scripts
+            bundles.Add(new ScriptBundle("~/bundles/vendor").Include(
+                    "~/scripts/jquery-2.1.1.min.js",
+                    "~/scripts/angular.min.js",
+                    "~/scripts/angular-animate.min.js",
+                    "~/scripts/angular-route.min.js",
+                    "~/scripts/angular-sanitize.min.js",
+                    "~/scripts/boostrap.min.js",
+                    "~/scripts/toastr.min.js",
+                    "~/scripts/moment.min.js",
+                    "~/scripts/ui-bootstrap-tpls-0.10.0.min.js",
+                    "~/scripts/spin.js"
+                ));
+
+            // Styles
+            bundles.Add(new StyleBundle("~/bundles/css").Include(
+                      "~/content/ie10mobile.css",
+                      "~/content/bootstrap.min.css",
+                      "~/content/font-awesome.min.css",
+                      "~/content/toastr.min.css",
+                      "~/content/customtheme.css",
+                      "~/content/styles.css",
+                      "~/content/breeze.directives.css"
+            ));
+
+            BundleTable.EnableOptimizations = true;
         }
     }
 }
--- HortiHoje.Web/app/layout/shell.js
+++ HortiHoje.Web/app/layout/shell.js
@@ -11,6 +11,7 @@
         var events = config.events;
         vm.busyMessage = 'Please wait ...';
         vm.isBusy = true;
+        vm.showSplash = true;
         vm.spinnerOptions = {
             radius: 40,
             lines: 7,
@@ -26,7 +27,9 @@
 
         function activate() {
             logSuccess('HortiHoje loaded!', null, true);
-            common.activateController([], controllerId);
+            common.activateController([], controllerId).then(function () {
+                vm.showSplash = false;
+            });
         }
 
         function toggleSpinner(on) { vm.isBusy = on; }
